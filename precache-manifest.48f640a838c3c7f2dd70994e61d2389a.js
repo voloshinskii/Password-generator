8f0e8c4397db71ab5605000102dc5cf127d69a1d
Max Voloshinskii <me@voloshinskii.ru>
2019-09-15 16:27:12 +0300

    Updates

---
+++ precache-manifest.48f640a838c3c7f2dd70994e61d2389a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "02db1867b6ca14dfde39",
+    "url": "/static/css/main.6d7a6ded.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "314b959801a1f3e182a1",
+    "url": "/static/js/2.fdd45e06.chunk.js"
+  },
+  {
+    "revision": "02db1867b6ca14dfde39",
+    "url": "/static/js/main.d9ec8fdf.chunk.js"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "314b959801a1f3e182a1",
+    "url": "/static/css/2.58678b29.chunk.css"
+  },
+  {
+    "revision": "d0ef5fd27a816389ea0eff67354cdc93",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Password-generator/precache-manifest.845590a35f092a2d5d42dd15d8066ffe.js"
+  "/precache-manifest.48f640a838c3c7f2dd70994e61d2389a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Password-generator/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
